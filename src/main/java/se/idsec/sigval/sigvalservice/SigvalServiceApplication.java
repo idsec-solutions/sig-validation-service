9e587f2188a2aadc3801ae836ebf33d0373b8561
Stefan Santesson <stefan@aaa-sec.com>
2020-06-17 11:04:13 +0200

    Base project

---
+++ src/main/java/se/idsec/sigval/sigvalservice/SigvalServiceApplication.java
@@ -0,0 +1,13 @@
+package se.idsec.sigval.sigvalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigvalServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SigvalServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/se/idsec/sigval/sigvalservice/SigvalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.idsec.sigval.sigvalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigvalServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
