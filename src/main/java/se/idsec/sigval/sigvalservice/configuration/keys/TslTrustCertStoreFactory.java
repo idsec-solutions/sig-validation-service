b72829e78a4ab2b3f577587e7e76b82501d7fe1c
Stefan Santesson <stefan@aaa-sec.com>
2023-01-30 11:58:03 +0100

    Fixed trusted certificate load

--- src/main/java/se/idsec/sigval/sigvalservice/configuration/keys/TslTrustCertStoreFactory.java
+++ src/main/java/se/idsec/sigval/sigvalservice/configuration/keys/TslTrustCertStoreFactory.java
@@ -16,37 +16,39 @@
 
 package se.idsec.sigval.sigvalservice.configuration.keys;
 
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.cert.CertStore;
+import java.security.cert.CertStoreParameters;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.CollectionCertStoreParameters;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ByteArrayEntity;
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.DERIA5String;
 import org.bouncycastle.asn1.cms.CMSObjectIdentifiers;
 import org.bouncycastle.asn1.cms.ContentInfo;
 import org.bouncycastle.asn1.cms.SignedData;
+import org.bouncycastle.asn1.x509.AccessDescription;
 import org.bouncycastle.asn1.x509.GeneralName;
-import se.swedenconnect.sigval.cert.utils.CertUtils;
-import se.swedenconnect.sigval.commons.utils.SVAUtils;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import se.swedenconnect.cert.extensions.SubjectInformationAccess;
 import se.swedenconnect.cert.extensions.data.OidName;
-
-import java.io.*;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.security.cert.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import se.swedenconnect.sigval.cert.utils.CertUtils;
+import se.swedenconnect.sigval.commons.utils.SVAUtils;
 
 @Slf4j
 @Getter
@@ -56,11 +58,13 @@ public class TslTrustCertStoreFactory {
   private final HttpClient httpClient;
   CertStore certStore;
 
-  public TslTrustCertStoreFactory(String policyRootLocation, HttpClient httpClient) throws IOException, CertificateException {
-    this.policyRoot = SVAUtils.getCertificate(IOUtils.toByteArray(new FileInputStream(new File(policyRootLocation))));
+  public TslTrustCertStoreFactory(String policyRootLocation, HttpClient httpClient)
+    throws IOException, CertificateException {
+    this.policyRoot = SVAUtils.getCertificate(IOUtils.toByteArray(new FileInputStream(policyRootLocation)));
     this.httpClient = httpClient;
     init();
   }
+
   public TslTrustCertStoreFactory(X509Certificate policyRoot, HttpClient httpClient) {
     this.policyRoot = policyRoot;
     this.httpClient = httpClient;
@@ -71,14 +75,16 @@ public class TslTrustCertStoreFactory {
     List<X509Certificate> certificateList = new ArrayList<>();
     try {
       SubjectInformationAccess siaExtension = CertUtils.getSIAExtension(policyRoot);
-      if (siaExtension == null){
+      if (siaExtension == null) {
         log.debug("The policy root certificate contains no SubjectInformationAccess Extension");
         return;
       }
       GeneralName generalName = Arrays.stream(siaExtension.getAccessDescriptions())
-        .filter(accessDescription -> accessDescription.getAccessMethod().getId().equals(OidName.id_pkix_ad_caRepository.getOid()))
-        .map(accessDescription -> accessDescription.getAccessLocation())
-        .findFirst().get();
+        .filter(accessDescription -> accessDescription.getAccessMethod()
+          .getId()
+          .equals(OidName.id_pkix_ad_caRepository.getOid()))
+        .map(AccessDescription::getAccessLocation)
+        .findFirst().orElseThrow(() -> new IllegalArgumentException("No CA repository access description available"));
       String location = ((DERIA5String) generalName.getName()).getString();
 
       HttpResponse httpResponse = httpClient.execute(new HttpGet(location));
@@ -89,21 +95,22 @@ public class TslTrustCertStoreFactory {
 
       ASN1InputStream ain = new ASN1InputStream(bytes);
       ContentInfo cmsContentInfo = ContentInfo.getInstance(ain.readObject());
-      if (!cmsContentInfo.getContentType().equals(CMSObjectIdentifiers.signedData)){
+      if (!cmsContentInfo.getContentType().equals(CMSObjectIdentifiers.signedData)) {
         throw new IOException("Illegal content type");
       }
       SignedData signedData = SignedData.getInstance(cmsContentInfo.getContent());
       Iterator<ASN1Encodable> iterator = signedData.getCertificates().iterator();
-      int certCount = 0;
-      log.debug("Collecting certificates from trust service store");
-      while (iterator.hasNext()){
-        try {
-          certCount++;
-          byte[] certByte = iterator.next().toASN1Primitive().getEncoded("DER");
-          X509Certificate cert = CertUtils.getCert(new ByteArrayInputStream(certByte));
-          certificateList.add(cert);
-          log.trace("Added certificate {} for {}", certCount, cert.getSubjectX500Principal());
-        } catch (Exception ex){
+
+      CertificateFactory cf = CertificateFactory.getInstance("X.509", "BC");
+      while (iterator.hasNext()) {
+        try (ByteArrayInputStream is = new ByteArrayInputStream(iterator.next().toASN1Primitive().getEncoded("DER"))) {
+          certificateList.add((X509Certificate) cf.generateCertificate(is));
+          if (log.isTraceEnabled()) {
+            log.trace("Added certificate {} for {}", certificateList.size(),
+              certificateList.get(certificateList.size() - 1).getSubjectX500Principal());
+          }
+        }
+        catch (Exception ex) {
           log.warn("Unable to decode certificate from signed data");
         }
       }
@@ -111,7 +118,8 @@ public class TslTrustCertStoreFactory {
       CertStoreParameters certStoreParameters = new CollectionCertStoreParameters(certificateList);
       certStore = CertStore.getInstance("Collection", certStoreParameters, "BC");
 
-    } catch (Exception ex){
+    }
+    catch (Exception ex) {
       log.warn("Unable to extract cert store from provided policy root certificate", ex);
       throw new RuntimeException("Unable to extract cert store from provided policy root certificate");
     }
